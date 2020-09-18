ca75b1fed8b91ad9a68994b5855976745276e25a
Ryan <shuttsie@gmail.com>
2020-09-18 08:59:03 -0500

    test

---
+++ src/views/Profile.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+import Highlight from '../components/Highlight';
+import Loading from '../components/Loading';
+import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react';
+
+export const ProfileComponent = () => {
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const [userMetadata, setUserMetadata] = useState(null);
+  useEffect(() => {
+    const getUserMetadata = async () => {
+      const domain = 'fiservseminars.us.auth0.com';
+
+      try {
+        const accessToken = await getAccessTokenSilently({
+          audience: `https://${domain}/api/v2/`,
+          scope: 'read:current_user',
+        });
+
+        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+
+        const metadataResponse = await fetch(userDetailsByIdUrl, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        const { user_metadata } = await metadataResponse.json();
+
+        setUserMetadata(user_metadata);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+
+    getUserMetadata();
+  }, []);
+
+  console.log(user);
+  console.log(userMetadata);
+
+  return (
+    isAuthenticated && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <h3>User Metadata</h3>
+        {userMetadata ? (
+          <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
+        ) : (
+          'No user metadata defined'
+        )}
+      </div>
+    )
+  );
+};
+
+export default withAuthenticationRequired(ProfileComponent, {
+  onRedirecting: () => <Loading />,
+});
