6b840f5a6c0fee4853f2039d228a34377c2c6369
Ryan <shuttsie@gmail.com>
2020-09-18 09:13:30 -0500

    api

--- src/views/ExternalApi.js
+++ src/views/ExternalApi.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
-import { Button, Alert } from "reactstrap";
-import Highlight from "../components/Highlight";
-import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
-import config from "../auth_config.json";
-import Loading from "../components/Loading";
+import React, { useState } from 'react';
+import { Button, Alert } from 'reactstrap';
+import Highlight from '../components/Highlight';
+import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react';
+import config from '../auth_config.json';
+import Loading from '../components/Loading';
 
-const { apiOrigin = "http://localhost:3001" } = config;
+const { apiOrigin = 'https://master.d1d56ff8e3932l.amplifyapp.com/' } = config;
 
 export const ExternalApiComponent = () => {
   const [state, setState] = useState({
     showResult: false,
-    apiMessage: "",
+    apiMessage: '',
     error: null,
   });
 
@@ -86,13 +86,13 @@ export const ExternalApiComponent = () => {
 
   return (
     <>
-      <div className="mb-5">
-        {state.error === "consent_required" && (
-          <Alert color="warning">
-            You need to{" "}
+      <div className='mb-5'>
+        {state.error === 'consent_required' && (
+          <Alert color='warning'>
+            You need to{' '}
             <a
-              href="#/"
-              class="alert-link"
+              href='#/'
+              class='alert-link'
               onClick={(e) => handle(e, handleConsent)}
             >
               consent to get access to users api
@@ -100,12 +100,12 @@ export const ExternalApiComponent = () => {
           </Alert>
         )}
 
-        {state.error === "login_required" && (
-          <Alert color="warning">
-            You need to{" "}
+        {state.error === 'login_required' && (
+          <Alert color='warning'>
+            You need to{' '}
             <a
-              href="#/"
-              class="alert-link"
+              href='#/'
+              class='alert-link'
               onClick={(e) => handle(e, handleLoginAgain)}
             >
               log in again
@@ -120,15 +120,15 @@ export const ExternalApiComponent = () => {
           the API's audience value.
         </p>
 
-        <Button color="primary" className="mt-5" onClick={callApi}>
+        <Button color='primary' className='mt-5' onClick={callApi}>
           Ping API
         </Button>
       </div>
 
-      <div className="result-block-container">
+      <div className='result-block-container'>
         {state.showResult && (
-          <div className="result-block" data-testid="api-result">
-            <h6 className="muted">Result</h6>
+          <div className='result-block' data-testid='api-result'>
+            <h6 className='muted'>Result</h6>
             <Highlight>
               <span>{JSON.stringify(state.apiMessage, null, 2)}</span>
             </Highlight>
